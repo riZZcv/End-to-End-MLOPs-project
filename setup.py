fc7d37e508d772b20d2680e928e16c84b7865603
riZZcv <ahmdrzwn888@gmail.com>
2025-09-25 22:50:28 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="riZZcv",
+    author_email="ahmdrzwn888@gmail.com",
+    packages=find_packages()
+)
